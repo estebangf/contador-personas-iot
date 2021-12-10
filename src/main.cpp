abdad097b86f9b8846347161735c33d0a5ea9614
leomestrit <leosui.91@gmail.com>
2021-12-10 16:22:08 -0300

    Añadiendo mqtt

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,29 @@
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include "Webpage.h"
+#include <MQTT.h>
+#include <HTTPClient.h> 
 
 WiFiClient client;
+MQTTClient clientmqtt;
+
+//--- Prototipo de Funciones --------
+void iniciarTrigger();
+float calcularDistancia();
 
 //--- Pines utilizados ---------------
 #define LEDAZUL 2 // LED azul de la placa
 
 #define TRIGGER 5 // Sensor ultrasonico
-#define ECHO 6    // Sensor ultrasonico
+#define ECHO 4    // Sensor ultrasonico
 //-----------------------------------------------
 
 //--- Constantes ---------------
 const float VEL_SONIDO = 34300.0; // Velocidad del sonido en cm/s
-const float UMBRAL = 30.0;        // Umbral maximo cuando no hay persona
+const float UMBRAL = 10.0;        // Umbral maximo cuando no hay persona
 const portTickType delayOneSeccond = 1000 / portTICK_RATE_MS;
+
+unsigned long lastMillis = 0;
 //-----------------------------------------------
 
 //--- CORE ------------------------------------
@@ -38,15 +47,35 @@ void tareaSensor(void *pvParameters);
 
 //--- CONFIGURACION WIFI ------------------------
 WebServer Server(80);
-const char *SSID = "Fibertel WiFi308 2.4GHz";
-const char *PASSWORD = "0041251608";
+const char *SSID = "leosui";
+const char *PASSWORD = "01345678";
 
 const char *DEVICE_NAME = "ESP32_THING";
 //-----------------------------------------------
 
+//-------- Conectando el MQTT ------------------
+
+
+void connect() {
+  Serial.printf("\nconnecting....");
+  while(!clientmqtt.connect("Broker IOT")) //Se conecta al broker de NodeRED con/sin usuario y pass
+  {
+    Serial.printf(".");
+    delay(1000);
+  }
+  Serial.print("\nconnected");
+  clientmqtt.subscribe("Leo"); //Poner su nombre
+}
+
+void messageReceived(String &topic, String &payload) //Recibe mensajes del topico
+{
+  Serial.println("incoming: " + topic + " - " + payload);
+}
+//--------------------------------------------------
+
 void setup()
 {
-
+  Serial.begin(115200);
   //--- Modo entrada/salida de los pines ----------
   pinMode(ECHO, INPUT);     // Entrada del Sensor ultrasonico
   pinMode(TRIGGER, OUTPUT); // Salida del Sensor ultrasonico
@@ -59,21 +88,30 @@ void setup()
   WiFi.begin(SSID, PASSWORD);
   while (WiFi.waitForConnectResult() != WL_CONNECTED)
   {
-    digitalWrite(LED, HIGH);
+    digitalWrite(LEDAZUL, HIGH);
     delay(100);
     Serial.print(".");
-    digitalWrite(LED, LOW);
+    digitalWrite(LEDAZUL, LOW);
     delay(100);
   }
-  digitalWrite(LED, LOW);
-  IPAddress ip(192, 168, 0, 125);
-  IPAddress gateway(192, 168, 0, 1);
+  digitalWrite(LEDAZUL, LOW);
+  IPAddress ip(192, 168, 43, 18);
+  IPAddress gateway(192, 168, 43, 1);
   IPAddress subnet(255, 255, 255, 0);
   IPAddress primaryDNS(8, 8, 8, 8);   //optional
   IPAddress secondaryDNS(8, 8, 4, 4); //optional
   WiFi.config(ip, gateway, subnet, primaryDNS, secondaryDNS);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+
+ //----------------CONECTANDO AL BROKER-------------------------------
+  
+  clientmqtt.begin("138.219.235.16", 6321, client); //cliente mqtt
+  //clientmqtt.onMessage(messageReceived);
+
+  connect();
+
   //-----------------------------------------------
 
   //--- CREACION DE TAREAS FREERTOS ---------------
@@ -81,16 +119,25 @@ void setup()
   //-----------------------------------------------
 
   //--- INICIALIZACION DEL SERVIDOR ---------------
-  Server.on("/", handleRoot);
-  Server.on("/readState", readState);
+  //Server.on("/", handleRoot);
+  //Server.on("/readState", readState);
   Server.begin();
   //-----------------------------------------------
 }
 
-void loop()
-{
-  Server.handleClient();
-  vTaskDelay(10 / portTICK_RATE_MS);
+void loop() {
+  clientmqtt.loop(); //Actualiza el estado del cliente MQTT, si o si tiene que estar en el loop
+
+  if (!clientmqtt.connected()) //Verifica que el client está conectado
+  {
+    connect();
+  }
+
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 5000) {
+    lastMillis = millis();
+    //clientmqtt.publish("Leo", "world"); //Poner su nombre
+  }
 }
 
 //--- FUNCIONES FREERTOS -------------------------
@@ -98,14 +145,13 @@ void tareaSensor(void *pvParameters)
 {
   while (true)
   {
-
     iniciarTrigger();                      // Preparamos el sensor de ultrasonidos
     float distancia = calcularDistancia(); // Obtenemos la distancia
     if (distancia < UMBRAL)                // Verificamos el umbral
     {
-      // Lanzamos alertas
+      clientmqtt.publish("Leo", "world", "musica" "river" "3");
     }
-    vTaskDelay(delayOneSeccond)
+    vTaskDelay(delayOneSeccond);
   }
 }
 //-----------------------------------------------
@@ -136,19 +182,4 @@ float calcularDistancia()
 
   return distancia;
 }
-//-----------------------------------------------
 
-//--- FUNCIONES SERVIDOR ------------------------
-void handleRoot()
-{
-  server.send(200, "text/html", WebpageCode);
-}
-void readState()
-{
-  String act_state = server.arg("state");
-
-  // CODIGO PERTINENTE...
-
-  server.send(200, "text/plane", state);
-}
-//-----------------------------------------------
